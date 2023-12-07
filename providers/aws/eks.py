dcc6a1b07849e193c7fbe11a1119cd1f56a14014
xneyder <xneyder@gmail.com>
2023-12-06 21:35:53 -0500

    Fix

--- providers/aws/eks.py
+++ providers/aws/eks.py
@@ -414,6 +414,7 @@ class EKS:
             cluster = self.eks_client.describe_cluster(name=cluster_name)[
                 "cluster"]
             tags = cluster.get("tags", {})
+            ftstack = "eks"
             if tags.get("ftstack", "eks") != "eks":
                 ftstack = "stack_"+tags.get("ftstack", "eks")
             
